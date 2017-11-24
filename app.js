cd5541cb0338beea8856db6d50b3d4005a0593eb
tangyang <tangyang@sogou-inc.com>
2017-11-24 13:19:35 +0800

    handsome

--- app.js
+++ app.js
@@ -11,7 +11,36 @@ io.on('connection',socket => {
     console.log('msg:' + msg)
     io.emit('back',msg)
   })
+
+  socket.on('typing',function(){
+    console.log('typing')
+    socket.emit('opp')
+  })
+
+  socket.on('type end',function(){
+    console.log('type end')
+    socket.emit('opp end')
+  })
+
+  let iterator = comet(socket);
+  setInterval(iterator.goNext,1000)
 })
+
+function comet(socket){
+    var i = 0;
+
+    return {
+      goNext(){
+          ++i;
+        if(i<=10){
+          console.log(`第${i}次推送数据`)
+          socket.emit('comet',`第${i}次推送数据`)
+        }else{
+          clearInterval(this)
+        }
+      }
+    }
+}
 http.listen(3000,function(){
   console.log('listen at 3000')
 })
